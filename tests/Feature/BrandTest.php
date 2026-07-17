85533ea00715f84022df770dba88a3817080bfff
Samuel Alderete Rayón <sammycoder@outlook.com>
2026-07-16 18:47:01 -0600

    🦄 refactor(tests): Brand and branches auth
    Context: API is now protected by "abilities:api-access" middleware
    Using Sanctum::actingAs($user, ['api-access']); instead
    $this->actingAs.
    
    Other changes:
    - Removed the use of dynamic properties $this->{prop} in the branches test

--- tests/Feature/BrandTest.php
+++ tests/Feature/BrandTest.php
@@ -1,31 +1,28 @@
 <?php
 
-use App\Models\Brand;
 use App\Models\User;
+use Laravel\Sanctum\Sanctum;
 
-beforeEach(function () {
-    $this->user = createUser();
-    $this->brand = Brand::factory()->create();
-});
+use function Pest\Laravel\getJson;
 
 describe('Brand authorization', function () {
     it('should require authentication for index', function () {
-        $response = $this->getJson(route('brands.index'));
+        $response = getJson(route('brands.index'));
         $response->assertStatus(401);
     });
 
     it('should require permission for index', function () {
         $user = User::factory()->create();
-        $this->actingAs($user, 'sanctum');
-        $response = $this->getJson(route('brands.index'));
+        Sanctum::actingAs($user, ['api-access']);
+        $response = getJson(route('brands.index'));
         $response->assertStatus(403);
     });
 
     it('should allow access to index with permission', function () {
         $user = User::factory()->create();
         $user->givePermissionTo('read-all-brands');
-        $this->actingAs($user, 'sanctum');
-        $response = $this->getJson(route('brands.index'));
+        Sanctum::actingAs($user, ['api-access']);
+        $response = getJson(route('brands.index'));
         $response->assertStatus(200);
     });
 });
