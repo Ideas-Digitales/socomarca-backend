26e189b6b61316a0a03a552839a4a7e631c04147
Samuel Alderete Rayón <sammycoder@outlook.com>
2026-07-16 19:29:40 -0600

    🦄 refactor(tests):  Auth abilities
    Context: API is now protected by "abilities:api-access" middleware
    - Using Sanctum::actingAs($user, ['api-access']); instead
    $this->actingAs.
    - Removed the use of $this->{function()} in benefit of
       native pest functions
    
    Updates tests:
    - Auth

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -10,28 +10,30 @@ use Laravel\Sanctum\Sanctum;
 use Mockery\MockInterface;
 use Tests\TestCase;
 
+use function Pest\Laravel\assertGuest;
+use function Pest\Laravel\deleteJson;
 use function Pest\Laravel\getJson;
 use function Pest\Laravel\patchJson;
 use function Pest\Laravel\postJson;
 
 uses(RefreshDatabase::class);
 
-test('usuario puede iniciar sesion con credenciales validas', function () {
-    // Preparación
+test('User can perform a successful login using valid credentials', function () {
+    // Arrange
     User::factory()->create([
         'rut' => '17260847-7',
         'password' => Hash::make('password123'),
         'is_active' => true,
     ]);
 
-    // Acción
-    $response = $this->postJson(route('auth.token.store'), [
+    // Act
+    $response = postJson(route('auth.token.store'), [
         'rut' => '17260847-7',
         'password' => 'password123',
         'device_name' => 'test-device',
     ]);
 
-    // Aserción
+    // Assert
     $response->assertStatus(200);
 
 });
@@ -47,7 +49,7 @@ test('User can login using formatted RUT', function ($dbRandomRut, $loginRut) {
     ]);
 
     // Act
-    $response = $this->postJson(route('auth.token.store'), [
+    $response = postJson(route('auth.token.store'), [
         'rut' => $loginRut,
         'password' => 'password123',
         'device_name' => 'test-device',
@@ -57,75 +59,75 @@ test('User can login using formatted RUT', function ($dbRandomRut, $loginRut) {
     $response->assertStatus(200);
 })->with('ValidForLoginRuts');
 
-test('usuario no puede iniciar sesion con credenciales invalidas', function () {
-    // Preparación
+test('User can\'t perform a successful login when using invalid credentials', function () {
+    // Arrange
     User::factory()->create([
         'rut' => '11111111-1',
         'password' => Hash::make('password123'),
     ]);
 
-    // Acción
-    $response = $this->postJson(route('auth.token.store'), [
+    // Act
+    $response = postJson(route('auth.token.store'), [
         'rut' => '11111111-1',
         'password' => 'wrongpassword',
         'device_name' => 'test-device',
     ]);
 
-    // Aserción
+    // Assert
     $response->assertStatus(401);
 });
 
-test('usuario inactivo no puede iniciar sesion', function () {
-    // Preparación
+test('Inactive user can\'t perform a successful login', function () {
+    // Arrange
     User::factory()->create([
         'rut' => '22222222-2',
         'password' => Hash::make('password123'),
         'is_active' => false,
     ]);
 
-    // Acción
-    $response = $this->postJson(route('auth.token.store'), [
+    // Act
+    $response = postJson(route('auth.token.store'), [
         'rut' => '22222222-2',
         'password' => 'password123',
         'device_name' => 'test-device',
     ]);
 
-    // Aserción
+    // Assert
     $response->assertStatus(401)
         ->assertJson([
             'message' => "Unauthorized",
         ]);
-    $this->assertGuest();
+    assertGuest();
 });
 
-test('usuario puede cerrar sesion', function () {
-    // Preparación
+test('User can destroy session', function () {
+    // Arrange
     $user = User::factory()->create([
         'rut' => '11111111-1',
         'password' => Hash::make('password123'),
     ]);
 
-    Sanctum::actingAs($user);
+    Sanctum::actingAs($user, ['api-access']);
 
-    // Acción
-    $response = $this->deleteJson(route('auth.token.destroy'));
+    // Act
+    $response = deleteJson(route('auth.token.destroy'));
 
-    // Aserción
+    // Assert
     $response->assertStatus(200);
 });
 
-test('usuario autenticado puede obtener su informacion', function () {
-    // Preparación
+test('Authenticated user can get its own information', function () {
+    // Arrange
     $user = User::factory()->create([
         'rut' => '33333333-3'
     ]);
     $user->assignRole('admin');
-    Sanctum::actingAs($user);
+    Sanctum::actingAs($user, ['api-access']);
 
-    // Acción
-    $response = $this->getJson('/api/users/' . $user->id);
+    // Act
+    $response = getJson('/api/users/' . $user->id);
 
-    // Aserción
+    // Assert
     $response->assertStatus(200);
 });
 
